73cd199c23e581e1a0e60990cfda24c7959687d3
amga <amgatech@gmail.com>
2017-08-23 14:37:43 +0200

    Initial commit

---
+++ src/main/java/com/example/spring/cloud/server/springcloudserver/SpringcloudserverApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring.cloud.server.springcloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringcloudserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudserverApplication.class, args);
+	}
+}
